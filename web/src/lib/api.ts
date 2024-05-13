9eee51ceba0360e67e7e6d456df7046b39e6f497
Sean Velasco <seanvelasco@ieee.org>
2024-05-14 05:10:00 +0800

    Add Accept headers to API calls

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,23 +1,39 @@
 import { BASE_API_URL } from "./constant"
 
 const count = async () => {
-    const response = await fetch(`${BASE_API_URL}/count`)
+    const response = await fetch(`${BASE_API_URL}/count`, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
     const data = await response.json()
     return data.count
 }
 
 const home = async () => {
-    const response = await fetch(BASE_API_URL)
+    const response = await fetch(BASE_API_URL, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
     return await response.json()
 }
 
 const images = async (image: string) => {
-    const response = await fetch(`${BASE_API_URL}/${image}`)
+    const response = await fetch(`${BASE_API_URL}/${image}`, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
     return await response.json()
 }
 
 const search = async (query: string) => {
-    const response = await fetch(`${BASE_API_URL}/search?query=${query}`)
+    const response = await fetch(`${BASE_API_URL}/search?query=${query}`, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
     return await response.json()
 }
 
