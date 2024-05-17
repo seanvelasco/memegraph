e6a149fd8eb4295de4ad8a60fa3c80f334647038
Sean Velasco <seanvelasco@ieee.org>
2024-05-18 04:00:26 +0800

    Remove web

--- web/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Header.module.css"
-import type { JSXElement } from "solid-js"
-
-const Header = (props: { children: JSXElement }) => (
-	<header class={styles.header}>{props.children}</header>
-)
-
-export default Header
