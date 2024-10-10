e043d72eb3ce1836bed7805ee5b9457d46fb0065
Sean Velasco <sean@sean.app>
2024-10-10 18:15:20 +0800

    Fix image page

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import sys
 from flask import Flask, render_template, g, request
 from flask_cors import CORS
 import psycopg
+from dotenv import load_dotenv
+
 if sys.platform == "darwin":
     from main import process_text
 
-DB_CONN_INFO = str(os.environ.get("DB_CONN_INFO"))
+load_dotenv()
+
+DB_CONN_INFO = str(os.environ.get("PG_CONN_STR"))
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -74,26 +78,25 @@ def count():
         count = conn.cursor().execute("SELECT COUNT(*) FROM images WHERE embedding IS NOT NULL").fetchone()[0]
         return {"count": count}
 
-@app.route("/<image>")
+@app.route("/<image_id>")
 def image(image_id):
     """Render details for a specific image and similar images."""
     with get_conn() as conn:
-        image = conn.cursor().execute("SELECT id, height, image FROM images WHERE id = %(id)s", {"id": image_id}).fetchone()
-        query = """
-            SELECT id, height, width, images.embedding <=> (SELECT embedding FROM images WHERE id = %(id)s) AS distance
-            FROM images WHERE id != %(id)s AND embedding IS NOT NULL
-            AND 1 - (images.embedding <=> (SELECT embedding FROM images WHERE id = %(id)s)) > 0.5
-            ORDER BY distance ASC LIMIT 30
-        """
-        images = conn.cursor().execute(query, {"id": image_id}).fetchall()
-        count = conn.cursor().execute("SELECT COUNT(*) FROM images WHERE embedding IS NOT NULL").fetchone()[0]
-
-        if not images:
-            return render_template('404.html', count=count), 404
-
-        images = [{'id': id, "distance": round(distance, 2), "similarity": similarity(distance), "height": height, "width": width} for (id, height, width, distance,) in images]
-
-        return render_template('image.html', image=image, images=images, count=count)
+        with conn.cursor() as cursor:
+            image = cursor.execute("SELECT id, height, width FROM images WHERE id = %(id)s", {"id": image_id}).fetchone()
+            query = """
+                SELECT id, height, width, images.embedding <=> (SELECT embedding FROM images WHERE id = %(id)s) AS distance
+                FROM images WHERE id != %(id)s AND embedding IS NOT NULL
+                AND 1 - (images.embedding <=> (SELECT embedding FROM images WHERE id = %(id)s)) > 0.5
+                ORDER BY distance ASC LIMIT 30
+            """
+            images = cursor.execute(query, {"id": image_id}).fetchall()
+            (count,) = cursor.execute("SELECT COUNT(*) as count FROM images WHERE embedding IS NOT NULL").fetchone()
+            if not images:
+                return render_template('404.html', count=count), 404
+            images = [{'id': id, "distance": round(distance, 2), "similarity": similarity(distance), "height": height, "width": width} for (id, height, width, distance) in images]
+            image = {'id': image[0], 'height': image[1], 'width': image[2]}
+            return render_template('image.html', image=image, images=images, count=count)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
